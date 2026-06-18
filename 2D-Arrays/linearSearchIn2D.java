baf5a9dd33add23d636fad2d26d123d19895ce19
sanku5003 <pariharsanket10@gmail.com>
2026-06-18 21:27:25 +0530

    performed linearSearch in java

---
+++ 2D-Arrays/linearSearchIn2D.java
@@ -0,0 +1,27 @@
+
+public class linearSearchIn2D {
+
+    public static  boolean linearSearch(int a[][], int key) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                if (key == a[i][j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int a[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        int key = 12;
+
+        if (linearSearch(a, key) == true) {
+            System.out.println("present");
+        } else {
+            System.out.println("not found");
+        }
+    }
+}
